f248a181794bcfeb5536f9a5a1d0002caa471639
Nikita Kubko <nkubko@ptsecurity.com>
2025-05-25 20:15:16 +1000

    перемещение с помощью мыши и стрелок

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -4,19 +4,60 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float rotationSpeed = 90.0f; // Скорость вращения
+    //public float rotationSpeed = 90.0f; // Скорость вращения
 
+    //void Update()
+    //{
+    //    // Вращение влево при нажатии клавиши A
+    //    if (Input.GetKey(KeyCode.A))
+    //    {
+    //        transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+    //    }
+    //    // Вращение вправо при нажатии клавиши D
+    //    if (Input.GetKey(KeyCode.D))
+    //    {
+    //        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    //    }
+    //}
+
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+    private float _rotationX = 0;
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+            body.freezeRotation = true;
+    }
     void Update()
     {
-        // Вращение влево при нажатии клавиши A
-        if (Input.GetKey(KeyCode.A))
+        if (axes == RotationAxes.MouseX)
         {
-            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
         }
-        // Вращение вправо при нажатии клавиши D
-        if (Input.GetKey(KeyCode.D))
+        else if (axes == RotationAxes.MouseY)
         {
-            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
         }
     }
 
